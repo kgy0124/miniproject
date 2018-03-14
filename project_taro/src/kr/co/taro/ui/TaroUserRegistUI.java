aa8888d585e5a3f7a3b1a86395dc35a9b35fa62d
LAPTOP-L2DDEPJC\eunj2n <eunj525@naver.com>
2018-03-14 20:53:18 +0900

    회원등록 수정

--- project_taro/src/kr/co/taro/ui/TaroUserRegistUI.java
+++ project_taro/src/kr/co/taro/ui/TaroUserRegistUI.java
@@ -20,29 +20,45 @@ public class TaroUserRegistUI extends BaseUI{
 		System.out.println(">> 회원등록을 선택하셨습니다 <<");
 		System.out.println("--------------------------");
 		
-		getStr(">> 회원등록을 하시겠습니까 ( Y : 회원등록 / N : 메뉴로 돌아가기 ) : ");
+		String result = "";
+		result = getStr(">> 회원등록을 하시겠습니까 ( Y : 회원등록 / N : 메뉴로 돌아가기 ) : ");
+		if(result.equalsIgnoreCase("N")) {
+			System.out.println(">> 메뉴로 돌아갑니다 <<");
+			return;
+		} else if(result.equalsIgnoreCase("Y")) {
+			TaroUser user = new TaroUser();
 		
-		// Y입력시
-		TaroUser user = new TaroUser();
-		
-		user.setId(getStr(">> 사용하실 ID를 입력해주세요 : "));
-		user.setName(getStr(">> 이름을 입력해주세요 : "));
-		user.setBirth(getStr(">> 생년월일을 입력해주세요 (ex 12341211) : "));
-			// 숫자 8자리가 아닐시 되묻기
-			getInt(">> 생년월일을 양식에 맞게 입력해주세요  (ex 12341211) : ");
-		user.setPassword(getStr(">> 설정하실 비밀번호를 입력해주세요 : "));
-		String respass = getStr(">> 비밀번호 재확인 : ");
-		if(user.getPassword() != respass) {
-			// 비밀번호가 같지 않을시 비밀번호 설정으로
-			System.out.println(">> 비밀번호 초기설정 화면으로 돌아갑니다 <<");
-			user.setPassword(getStr(">> 설정하실 비밀번호를 입력해주세요 : "));
+			user.setId(getStr(">> 사용하실 ID를 입력해주세요 : "));
+			user.setName(getStr(">> 이름을 입력해주세요 : "));
+
+			String birth = "";
+			while(true) {
+				birth = getStr(">> 생년월일을 입력해주세요 (ex 12341211) : ");
+				if(birth.length() != 8) {
+					System.out.println(">> 생년월일을 양식에 맞게 입력해주세요 <<");
+				} else break;
+			}
+			user.setBirth(birth);	
 			
+			String pass = "";
+			while(true) {
+				pass = (getStr(">> 생성하실 비밀번호를 입력해주세요 : "));
+				String respass = getStr(">> 비밀번호 재확인 : ");
+				if(pass == respass) {
+					System.out.println(">> 비밀번호가 확인되었습니다 <<");
+					return;
+				} 
+				System.out.println(">> 올바르지 않은 비밀번호를 입력하셨습니다 <<");
+				System.out.println(">> 비밀번호 초기설정 화면으로 돌아갑니다 <<");
+				break;
+			}
+			user.setPassword(pass);
+			
+			System.out.println(">> 회원등록중... <<");
+		
+			mapper.userInsert(user);
+		
+			System.out.println(">>회원 등록이 완료 되었습니다 <<");
 		}
-		
-		System.out.println(">>회원 등록이 완료 되었습니다 <<");
-		
-		// N 입력시
-		System.out.println(">> 메뉴로 돌아갑니다 <<");
-		
 	}
 }
