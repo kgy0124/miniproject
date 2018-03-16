ef73b8bfbaa8556060abdde2a27e8732fb0404f5
LAPTOP-L2DDEPJC\eunj2n <eunj525@naver.com>
2018-03-16 18:53:59 +0900

    로그아웃 수정했습니다

--- project_taro/src/kr/co/taro/detailedUI/LogOut.java
+++ project_taro/src/kr/co/taro/detailedUI/LogOut.java
@@ -7,22 +7,31 @@ import kr.co.taro.ui.TaroUI;
 public class LogOut extends BaseUI {
 
 	public void service() {
-		System.out.println("==============================================");
-		System.out.println("로그 아웃 하시겠습니까 (Y/N)");
-		String ch=sc.nextLine();
+		System.out.println("==================================================================");
+		System.out.println("                         [사용자 로그아웃]                           ");
+		System.out.println("------------------------------------------------------------------");
 		
-		if(ch.equalsIgnoreCase("Y")) {
-			TaroUI.usertmp = null;
-			TaroUI.usertmp = new TaroUser();
-			System.out.println("로그아웃 되었습니다.");
-		}else if(ch.equalsIgnoreCase("N")) {
-			System.out.println("메뉼로 돌아갑니다.");
-			return;
+		while(true) {
+			String result = getStr(">> 로그아웃을 진행하시겠습니까 ( Y : 회원 로그아웃 / N : 메뉴로 돌아가기 ) : ");
+			if(result.equalsIgnoreCase("Y")) {
+				TaroUI.usertmp = null;
+				TaroUI.usertmp = new TaroUser();
+				System.out.println(".......................................");
+				System.out.println(">>       정상적으로 로그아웃 되었습니다                  <<");
+				System.out.println(".......................................");
+				break;
+			}else if(result.equalsIgnoreCase("N")) {
+				System.out.println(".......................................");
+				System.out.println(">>           메뉴로 돌아갑니다                             <<");
+				System.out.println(".......................................");
+				return;
 			}
-		else {
-			System.out.println("올바른 값이 아닙니다.");
-			return;
+			else {
+				System.out.println(".......................................");
+				System.out.println(">>    올바른 값이 아닙니다. 다시 입력해주세요         <<");
+				System.out.println(".......................................");
+				continue;
+			}
 		}
 	}
-
 }
