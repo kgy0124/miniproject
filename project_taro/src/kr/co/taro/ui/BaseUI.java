1919328f384d40d2147893ac870dfbf5abb44aa4
DESKTOP-EMMG8UD\MacBook <blanc_-@naver.com>
2018-03-16 11:07:50 +0900

    논뷰, 리얼타로뷰에서 베이스ui로  메서드 분리

--- project_taro/src/kr/co/taro/ui/BaseUI.java
+++ project_taro/src/kr/co/taro/ui/BaseUI.java
@@ -16,4 +16,28 @@ public abstract class BaseUI {
 		System.out.print(msg);
 		return sc.nextLine();
 	}
+	public void taromenu() {
+		while(true) {
+			String yn ="";
+			yn = getStr(">> 타로를 보시겠습니까?  ( Y : 타로보기 / N : 메뉴로 돌아가기 ) : ");
+			
+			if(yn.equalsIgnoreCase("N")) {
+				System.out.println();
+				System.out.println(">> 메뉴로 돌아갑니다. <<");
+				System.out.println();
+			    return;
+			}else if(yn.equalsIgnoreCase("Y")) {
+				RealTaroViewUI aaa = new RealTaroViewUI();
+				aaa.service();
+				break;
+			}else {
+				System.out.println(">> 올바른 값이 아닙니다. 다시 골라주세요 <<");
+				System.out.println();
+				continue;
+			}
+	    }//while
+	}	
+	
+	
+	
 }
